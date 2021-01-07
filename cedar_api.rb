f7dec6e886d1e4f92b246251a471e48df14dca89
Yunwei Wang <yunwwang@gmail.com>
2021-01-07 13:09:50 -0600

    Add mapping from artifact to fhir

--- cedar_api.rb
+++ cedar_api.rb
@@ -31,13 +31,13 @@ namespace '/fhir' do
   end
 
   get '/Citation/?:id?' do
-    id = "citation-#{params[:id].nil? ? '323' : params[:id]}"
-    get_resource(id, use_fhir_parser:false)
+    id = params[:id].nil? ? 'citation-323' : params[:id]
+    get_resource(id)
   end
 
   get '/EvidenceReport/?:id?' do
-    id = "evidencereport-#{params[:id].nil? ? 'cervical-cancer' : params[:id]}"
-    get_resource(id, use_fhir_parser:false)
+    id = params[:id].nil? ? 'evidencereport-cervical-cancer' : params[:id]
+    get_resource(id)
   end
 
   get '/Group/?:id?' do
@@ -46,14 +46,144 @@ namespace '/fhir' do
   end
 
   get '/PlanDefinition/?:id?' do
-    binding.pry
-    id = "plandefinition-#{params[:id].nil? ? 'cervical-cancer' : params[:id]}"
+    id = params[:id].nil? ? 'plandefinition-cervical-cancer' : params[:id]
     get_resource(id)
   end
 
   def get_resource(id, use_fhir_parser: true)
+    return get_resource_from_artifact(id) unless id.nil?
+
     json = File.read("resources/#{id}.json")
     resource = use_fhir_parser ? FHIR.from_contents(json) : JSON.parse(json)
     resource.to_json
+  end
+
+  def get_resource_from_artifact(id)
+    artifact = Artifact[id]
+    return nil if artifact.nil?
+
+    artifact_type = artifact[:artifact_type]
+
+    case artifact_type
+    when 'specific_recommendation'
+      create_plan_definition(artifact)
+    when 'general_recommendation'
+      create_general_recommendation(artifact)
+    when 'tool'
+      create_citation(artifact)
+    end
+  end
+
+  def create_citation(artifact)
+    remote_id = artifact[:remote_id]
+    citation = {
+      id: artifact[:id],
+      identifier: [
+        {
+          system: 'https://www.uspreventiveservicestaskforce.org/tool',
+          value: remote_id[12..]
+        }
+      ],
+      title: artifact[:title],
+      status: 'active',
+      webLocation: {
+        url: artifact[:url]
+      }
+    }
+
+    citation.to_json
+  end
+
+  def create_general_recommendation(artifact)
+    remote_id = artifact[:remote_id]
+
+    evidence_rpt = {
+      id: artifact[:id],
+      identifier: [
+        {
+          system: 'https://www.uspreventiveservicestaskforce.org/general-recommendation',
+          value: remote_id[10..]
+        }
+      ],
+      title: artifact[:title],
+      status: 'active',
+      type: {
+        coding: [
+          {
+            system: 'http://terminology.hl7.org/CodeSystem/evidence-report-type',
+            code: 'text-structured'
+          }
+        ]
+      },
+      section: [
+        {
+          text: {
+            status: 'generated',
+            div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">#{artifact[:description]}</div>"
+          }
+        }
+      ]
+    }
+
+    evidence_rpt.to_json
+  end
+
+  def create_plan_definition(artifact)
+    remote_id = artifact[:remote_id]
+
+    plan_def = FHIR::PlanDefinition.new(
+      id: artifact[:id],
+      meta: {
+        profile: [
+          'http://hl7.org/fhir/uv/cpg/StructureDefinition/cpg-computableplandefinition'
+        ]
+      },
+      text: {
+        status: 'generated',
+        div: "<div xmlns=\"http://www.w3.org/1999/xhtml\">#{artifact[:description]}</div>"
+      },
+      extension: [
+        {
+          url: 'http://hl7.org/fhir/uv/cpg/StructureDefinition/cpg-knowledgeCapability',
+          valueCode: 'computable'
+        },
+        {
+          url: 'http://hl7.org/fhir/uv/cpg/StructureDefinition/cpg-knowledgeRepresentationLevel',
+          valueCode: 'narrative'
+        },
+        {
+          url: 'http://hl7.org/fhir/uv/cpg/StructureDefinition/cpg-enabled',
+          valueBoolean: true
+        }
+      ],
+      url: "http://example.com/fhir/PlanDefinition/#{artifact[:id]}",
+      identifier: [
+        {
+          system: 'https://www.uspreventiveservicestaskforce.org/specific-recommendation',
+          value: remote_id[10..]
+        }
+      ],
+      title: artifact[:title],
+      type: {
+        coding: [
+          {
+            system: 'http://terminology.hl7.org/CodeSystem/plan-definition-type',
+            code: 'eca-rule',
+            display: 'ECA Rule'
+          }
+        ]
+      },
+      status: 'active',
+      experimental: false,
+      publisher: 'USPSTF',
+      description: artifact[:description],
+      action: [
+        {
+          title: artifact[:title]
+        }
+      ]
+    )
+
+    plan_def.to_json
   end
 end
