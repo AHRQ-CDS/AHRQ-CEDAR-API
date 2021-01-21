96a07206f540390ecae7073b3727eb6084a297ef
Marc Hadley <mhadley@mitre.org>
2021-01-21 16:25:29 -0500

    Fix unit tests

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -3,6 +3,14 @@
 # Very simple fixture to start with: one table with single entry
 DB.create_table :artifacts do
   primary_key :id
+  foreign_key :repository_id, :repositories
   string :remote_identifier
 end
-DB[:artifacts].insert(id: 1, remote_identifier: '1')
+
+DB.create_table :repositories do
+  primary_key :id
+  string :name
+end
+
+DB[:repositories].insert(id: 1, name: 'USPSTF')
+DB[:artifacts].insert(id: 1, remote_identifier: '1', repository_id: 1)
