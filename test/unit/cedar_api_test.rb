3f5bca05d8f8a64a5ee5d5bfc543159b3c1119a4
Yunwei Wang <yunwwang@gmail.com>
2021-02-10 13:19:30 -0600

    Convert all tests to Spec

--- test/unit/cedar_api_test.rb
+++ test/unit/cedar_api_test.rb
@@ -10,7 +10,7 @@ describe 'cedar_api' do
     it 'returns count of artifacts' do
       get '/'
       assert last_response.ok?
-      assert_equal 'Artifact count: 1', last_response.body
+      assert_equal 'Artifact count: 3', last_response.body
     end
 
     it 'returns not found when endpoint does not exist' do
@@ -36,7 +36,7 @@ describe 'cedar_api' do
   end
 
   describe '/fhir/Citation endpoint' do
-    it 'returns resource when read with id' do
+    it 'supports read with id' do
       get '/fhir/Citation/abc-1'
 
       assert last_response.ok?
@@ -50,76 +50,41 @@ describe 'cedar_api' do
       get '/fhir/Citation/1000'
       assert last_response.not_found?
     end
-  end
-end
 
-class CedarApiTest < MiniTest::Test
-  include Rack::Test::Methods
-  include CedarApi::TestHelper
+    it 'supports search by _content' do
+      get '/fhir/Citation?_content=cancer'
+      assert last_response.ok?
+      record = JSON.parse(last_response.body)
+      assert_equal('Bundle', record['resourceType'])
+      assert_equal('searchset', record['type'])
+      assert_equal(1, record['total'])
+    end
 
-  def test_root_return_count
-    get '/'
-    assert last_response.ok?
-    assert_equal 'Artifact count: 3', last_response.body
-  end
+    it 'supports search by title' do
+      get '/fhir/Citation?title=diabetes'
+      assert last_response.ok?
+      record = JSON.parse(last_response.body)
+      assert_equal('Bundle', record['resourceType'])
+      assert_equal('searchset', record['type'])
+      assert_equal(1, record['total'])
+    end
 
-  def test_citation_found
-    get '/fhir/Citation/abc-1'
-    assert last_response.ok?
-    record = JSON.parse(last_response.body)
-    assert_equal('Citation', record['resourceType'])
-    assert_equal('abc-1', record['id'])
-    assert_equal('active', record['status'])
-  end
+    it 'supports search by title with multiple OR' do
+      get '/fhir/Citation?title=cancer,diabetes'
+      assert last_response.ok?
+      record = JSON.parse(last_response.body)
+      assert_equal('Bundle', record['resourceType'])
+      assert_equal('searchset', record['type'])
+      assert_equal(2, record['total'])
+    end
 
-  def test_citation_saerch_by_content
-    get '/fhir/Citation?_content=cancer'
-    assert last_response.ok?
-    record = JSON.parse(last_response.body)
-    assert_equal('Bundle', record['resourceType'])
-    assert_equal('searchset', record['type'])
-    assert_equal(1, record['total'])
-  end
-
-  def test_citation_saerch_by_title
-    get '/fhir/Citation?title=diabetes'
-    assert last_response.ok?
-    record = JSON.parse(last_response.body)
-    assert_equal('Bundle', record['resourceType'])
-    assert_equal('searchset', record['type'])
-    assert_equal(1, record['total'])
-  end
-
-  def test_citation_saerch_by_title_or
-    get '/fhir/Citation?title=cancer,diabetes'
-    assert last_response.ok?
-    record = JSON.parse(last_response.body)
-    assert_equal('Bundle', record['resourceType'])
-    assert_equal('searchset', record['type'])
-    assert_equal(2, record['total'])
-  end
-
-  def test_citation_saerch_by_title_contains
-    get '/fhir/Citation?title:contains=diabetes'
-    assert last_response.ok?
-    record = JSON.parse(last_response.body)
-    assert_equal('Bundle', record['resourceType'])
-    assert_equal('searchset', record['type'])
-    assert_equal(2, record['total'])
-  end
-
-  def test_root_not_found
-    get '/foobar'
-    assert last_response.not_found?
-  end
-
-  def test_other_resource_not_found
-    get '/fhir/EvidenceReport/1000'
-    assert last_response.not_found?
-  end
-
-  def test_citation_not_found
-    get '/fhir/Citation/1000'
-    assert last_response.not_found?
+    it 'support search by title with :contains modifier' do
+      get '/fhir/Citation?title:contains=diabetes'
+      assert last_response.ok?
+      record = JSON.parse(last_response.body)
+      assert_equal('Bundle', record['resourceType'])
+      assert_equal('searchset', record['type'])
+      assert_equal(2, record['total'])
+    end
   end
 end
