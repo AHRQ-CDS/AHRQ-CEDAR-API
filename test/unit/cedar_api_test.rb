aa60675491544f1b46a79fc779f17f15d9b1671f
Yunwei Wang <yunwwang@gmail.com>
2021-02-10 15:42:36 -0600

    Check the bundle content instead of bundle.total

--- test/unit/cedar_api_test.rb
+++ test/unit/cedar_api_test.rb
@@ -1,16 +1,30 @@
 # frozen_string_literal: true
 
 require_relative '../test_helper'
+require_relative '../../database/models'
 
 describe 'cedar_api' do
   include Rack::Test::Methods
   include CedarApi::TestHelper
 
+  def assert_bundle
+    assert last_response.ok?
+    bundle = FHIR.from_contents(last_response.body)
+
+    refute_nil bundle
+    assert bundle.is_a?(FHIR::Bundle)
+    assert_equal 'searchset', bundle.type
+    assert_equal bundle.total, bundle.entry.size
+    assert bundle.entry.size.positive?
+
+    bundle
+  end
+
   describe 'root' do
     it 'returns count of artifacts' do
       get '/'
       assert last_response.ok?
-      assert_equal 'Artifact count: 3', last_response.body
+      assert_equal "Artifact count: #{Artifact.count}", last_response.body
     end
 
     it 'returns not found when endpoint does not exist' do
@@ -40,10 +54,12 @@ describe 'cedar_api' do
       get '/fhir/Citation/abc-1'
 
       assert last_response.ok?
-      record = JSON.parse(last_response.body)
-      assert_equal('Citation', record['resourceType'])
-      assert_equal('abc-1', record['id'])
-      assert_equal('active', record['status'])
+      resource = FHIR.from_contents(last_response.body)
+
+      refute_nil resource
+      assert resource.is_a?(FHIR::Citation)
+      assert_equal 'abc-1', resource.id
+      assert_equal 'active', resource.status
     end
 
     it 'returns not found when read with invalid id' do
@@ -53,38 +69,37 @@ describe 'cedar_api' do
 
     it 'supports search by _content' do
       get '/fhir/Citation?_content=cancer'
-      assert last_response.ok?
-      record = JSON.parse(last_response.body)
-      assert_equal('Bundle', record['resourceType'])
-      assert_equal('searchset', record['type'])
-      assert_equal(1, record['total'])
+      bundle = assert_bundle
+
+      assert bundle.entry.all? do |entry|
+        entry.resource.title.downcase.include?('cancer') ||
+          entry.resource.description.downcase.include?('cancer')
+      end
     end
 
     it 'supports search by title' do
       get '/fhir/Citation?title=diabetes'
-      assert last_response.ok?
-      record = JSON.parse(last_response.body)
-      assert_equal('Bundle', record['resourceType'])
-      assert_equal('searchset', record['type'])
-      assert_equal(1, record['total'])
+      bundle = assert_bundle
+      assert bundle.entry.all? do |entry|
+        entry.resource.title.downcase.start_with?('diabetes')
+      end
     end
 
     it 'supports search by title with multiple OR' do
       get '/fhir/Citation?title=cancer,diabetes'
-      assert last_response.ok?
-      record = JSON.parse(last_response.body)
-      assert_equal('Bundle', record['resourceType'])
-      assert_equal('searchset', record['type'])
-      assert_equal(2, record['total'])
+      bundle = assert_bundle
+      assert bundle.entry.all? do |entry|
+        entry.resource.title.downcase.start_with?('diabetes') ||
+          entry.resource.title.downcase.start_with?('cancer')
+      end
     end
 
     it 'support search by title with :contains modifier' do
       get '/fhir/Citation?title:contains=diabetes'
-      assert last_response.ok?
-      record = JSON.parse(last_response.body)
-      assert_equal('Bundle', record['resourceType'])
-      assert_equal('searchset', record['type'])
-      assert_equal(2, record['total'])
+      bundle = assert_bundle
+      assert bundle.entry.all? do |entry|
+        entry.resource.title.downcase.include?('diabetes')
+      end
     end
   end
 end
