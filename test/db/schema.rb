ccabfdea1f9130726af1ea722544b7d28ab7409e
Diana Eastman <deastman@mitre.org>
2021-10-25 09:29:52 -0400

    Add alias to schema.

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_130500) do
+ActiveRecord::Schema.define(version: 2021_10_22_153224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2021_09_23_130500) do
   end
 
   create_table "repositories", force: :cascade do |t|
+    t.string "alias"
     t.string "name"
     t.string "fhir_id"
     t.string "home_page"
